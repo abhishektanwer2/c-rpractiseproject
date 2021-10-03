6d14603c4f593533dee6041fdc76161c9c6d0e78
Abhishek Tanwer <abhishektanwer2@gmail.com>
2021-10-03 13:34:41 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace c_Projects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World ");
+            List<int> list = new();
+            list.Add(1);
+            list.Add(23);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+
+            }
+
+
+        }
+    }
+}
