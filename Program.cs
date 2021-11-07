dd8daed3ed4c0cb0fd8d3a2e4abbccd6fc2df18e
Abhishek Tanwer <abhishektanwer2@gmail.com>
2021-11-07 13:35:08 -0700

    chaning some value

--- Program.cs
+++ Program.cs
@@ -11,9 +11,15 @@ namespace c_Projects
         {
             Console.WriteLine("Hello World ");
             List<int> list = new();
+<<<<<<< ours
             list.Add(1);
             list.Add(1);
             list.Add(1);
+=======
+            list.Add(16); s
+            list.Add(16);
+            list.Add(15);
+>>>>>>> theirs
             foreach (var item in list)
             {
                 Console.WriteLine(item);
